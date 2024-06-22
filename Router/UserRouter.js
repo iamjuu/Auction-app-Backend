46830b34958935d35d261487982d122a282e781f
muhammed ajmal <muhammedajmalcc6424094@gmail.com>
2024-06-23 01:01:51 +0530

    opt checking

--- Router/UserRouter.js
+++ Router/UserRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 // const { propertyImageupload } = require("../middleware/multer");
 
 const Signup = require("../controller/user/signupController");
-const otp    = require("../controller/user/Otp Controller")
+const otp    = require("../controller/user/signupController")
 
 router.post("/signup", Signup.SignupPost)
       .post("/otp",otp.otpPost)
--- controller/user/signupController.js
+++ controller/user/signupController.js
@@ -5,7 +5,8 @@ const sendmail = require('../../utils/otp');
 module.exports = {
   SignupPost: async (req, res) => {
     try {
-      const { Name, email, password, type } = req.body;
+      var GenerateOTP=''
+      const { Name, email, password, type } = req.body
 
       if (type === "User") {
         const Data = {
@@ -14,11 +15,11 @@ module.exports = {
           password: password,
         };
         req.session.email = email;
-        console.log(  req.session.email,'session email  in user');
+        console.log(  req.session.email,'session email  in user')
 
-        const newData = new signupData(Data);
+        const newData = new signupData(Data)
         await newData.save();
-        res.status(200).json({ success: true });
+        res.status(200).json({ success: true })
       } else if (type === "Agent") {
         const Data = {
           name: Name,
@@ -26,15 +27,13 @@ module.exports = {
           password: password,
         };
         req.session.email = email;
-        console.log(  req.session.email,'session email  in agent ');
-        const newData = new AgentsignupDatas(Data);
-        await newData.save();
-        res.status(200).json({ success: true });
+        console.log(  req.session.email,'session email  in agent ')
+        const newData = new AgentsignupDatas(Data)
+        await newData.save()
+        res.status(200).json({ success: true })
       } else {
         res.status(400).json({ success: false, message: 'Invalid type' });
       }
-
-
       // OTP sending
       const emailToSend = req.session.email;
       console.log(emailToSend, 'session email'); // Debugging line
@@ -42,21 +41,17 @@ module.exports = {
       if (!emailToSend) {
         throw new Error("Email not found in session");
       }
-
-      const generateOTP = Math.floor(1000 + Math.random() * 9000);
+    const  generateOTP = Math.floor(1000 + Math.random() * 9000);
       console.log(generateOTP,'otp');
-      req.session.otp = generateOTP;
-
-
-
-
-
-      
+      req.session.otp = generateOTP;      
       await sendmail(emailToSend, generateOTP);
-
     } catch (error) {
       console.log(error, "error in signup post");
       res.status(500).json({ success: false, message: 'Internal server error' });
     }
   },
+  otpPost:async(req,res)=>{
+    console.log(req.body,' opt data');
+    
+    },
 };
